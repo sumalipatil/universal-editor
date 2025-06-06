b711ddd1b38ae6395f8a430c39707fbfa3907288
Suresh MaliPatil <sumalipatil@deloitte.com>
2025-06-06 10:49:06 +0530

    product info updates

--- blocks/product-info/product-info.js
+++ blocks/product-info/product-info.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
   // Create Button
   const button = document.createElement('button');
-  button.textContent = 'AEM to App builder';
+  button.textContent = 'Create Customer via API Mesh';
   button.style.padding = '1em';
   button.style.marginBottom = '1em';
   block.appendChild(button);
@@ -15,24 +15,33 @@ export default function decorate(block) {
 
   // Button click handler
   button.addEventListener('click', async () => {
-    const endpoint = 'https://27420-auspost-integratiton.adobeioruntime.net/api/v1/web/shipping/fetch-shipping-prices';
+    const endpoint = 'https://edge-sandbox-graph.adobe.io/api/0804747e-2944-4ef2-b5f7-e1b7a1d6bc32/graphql';
+    const token = 'f75115a1f5c64e61a50e050543da9545';
 
-    const token = 'eyJhbGciOiJSUzI1NiIsIng1dSI6Imltc19uYTEta2V5LWF0LTEuY2VyIiwia2lkIjoiaW1zX25hMS1rZXktYXQtMSIsIml0dCI'
-     + '6ImF0In0.eyJpZCI6IjE3NDkxMjYyNTgwMDdfNzY5YjQ4OTUtODhkMS00Y2FkLTg3OGUtMGFkYzdlMGNmZjliX3V3MiIsIm9yZyI6IjIyRkFERTAw'
-     + 'NTcyMUY4MjU3RjAwMDEwMUBBZG9iZU9yZyIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJjbGllbnRfaWQiOiI3NzE4NWExZTdkZDE0M2NiYWE0Mzc2'
-     + 'ODM3MTc4MGM4MiIsInVzZXJfaWQiOiI5OTNCMjI3RTY4MkIyQkVDMEE0OTVDMjdAdGVjaGFjY3QuYWRvYmUuY29tIiwiYXMiOiJpbXMtbmExIiwiYW'
-     + 'FfaWQiOiI5OTNCMjI3RTY4MkIyQkVDMEE0OTVDMjdAdGVjaGFjY3QuYWRvYmUuY29tIiwiY3RwIjozLCJtb2kiOiJjZDA4MDQ1IiwiZXhwaXJlc19p'
-     + 'biI6Ijg2NDAwMDAwIiwic2NvcGUiOiJhZG9iZWlvX2FwaSxvcGVuaWQscmVhZF9jbGllbnRfc2VjcmV0LEFkb2JlSUQsYWRkaXRpb25hbF9pbmZvLnJ'
-     + 'vbGVzLG1hbmFnZV9jbGllbnRfc2VjcmV0cyxyZWFkX29yZ2FuaXphdGlvbnMsYWRkaXRpb25hbF9pbmZvLnByb2plY3RlZFByb2R1Y3RDb250ZXh0L'
-     + 'GV2ZW50X3JlY2VpdmVyX2FwaSIsImNyZWF0ZWRfYXQiOiIxNzQ5MTI2MjU4MDA3In0.RuwJ_vZYLmBBVbgTphnB4yVPsiBoA7_Oj_H21HKsGYzDs17'
-     + 'O-DRtTwflmT3EZPHwMlcLIx8J0jWi1i2sIg0H-e179Kdc3Xiy8k_mFjpcXR9i8SEWGeGMdXUB0obGC_fSBpmfx3jQ1wMDTtKzOA27p2PURM4lSxOvx'
-     + 'Y9hhRmCCBqmR_9wkzGTtDoSgqxIlc_cKjueP-JOUcBNvVcFWzvWRMXPF6x5gnvfWWczaezbAF_tc6YWEoy3QblLbQzKN9S4r_19zS5Icu-4FWJDn0F'
-     + 'g9Ofi7KW2FfJXnklClSH71Dqig8ScoHYxkzFkP_9XUW01v6Z47KuwTTBdA7I8RiNR1g';
+    const firstname = block.dataset.firstname || 'John';
+    const lastname = block.dataset.lastname || 'Doe';
+    const email = block.dataset.email || 'john.doe@example.com';
+    const password = block.dataset.password || 'MySecurePassword123';
 
-    const query = {
-      title: 'I am Hero',
-      userId: 5,
-    };
+    const query = `
+      mutation {
+        createCustomerV2(
+          input: {
+            firstname: "${firstname}"
+            lastname: "${lastname}"
+            email: "${email}"
+            password: "${password}"
+          }
+        ) {
+          customer {
+            firstname
+            lastname
+            email
+            is_subscribed
+          }
+        }
+      }
+    `;
 
     try {
       const response = await fetch(endpoint, {
@@ -40,7 +49,6 @@ export default function decorate(block) {
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`,
-          'x-gw-ims-org-id': '22FADE005721F8257F000101@AdobeOrg',
         },
         body: JSON.stringify({ query }),
       });
@@ -57,3 +65,4 @@ export default function decorate(block) {
     }
   });
 }
+
